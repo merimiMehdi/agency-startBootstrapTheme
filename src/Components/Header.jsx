929c8b5b9c1983fb4fdcb6ac39fab413b9f9a88e
Mehdi MERIMI <mhidomer@gmail.com>
2023-11-12 17:14:59 +0100

    main change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Header() {
     return(
     
-        <header class="masthead">
-            <div class="container">
-                <div class="masthead-subheading">Welcome To Our Studio!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+        <header className="masthead">
+            <div className="container">
+                <div className="masthead-subheading">Welcome To Our Studio!</div>
+                <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
         </header>
     )
